406aa8ef43f4765359c249580d48949f719474dc
Gonza Rascon <gonzarascon@gmail.com>
2021-08-23 00:46:47 -0300

    Upgraded with template

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -20,8 +20,11 @@ task("accounts", "Prints the list of accounts", async (_args, hre) => {
 export default {
   solidity: "0.8.0",
   typechain: {
-    outDir: "./@types",
+    outDir: "../frontend/@types",
     target: "truffle-v5",
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+  },
+  paths: {
+    artifacts: "../frontend/artifacts", // We output artifacts to the frontend so we can use the ABI and it's address locally.
   },
 } as HardhatUserConfig;
