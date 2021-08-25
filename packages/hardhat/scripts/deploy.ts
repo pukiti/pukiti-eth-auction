213d5ce022599741f2d19829029cd7419eef1552
gonzarascon <gonzarascon@gmail.com>
2021-08-25 00:47:34 -0300

    Setup hardhat package for auction contract

--- packages/hardhat/scripts/deploy.ts
+++ packages/hardhat/scripts/deploy.ts
@@ -1,46 +1,98 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { Contract } from "ethers";
-import { config, ethers } from "hardhat";
+/* eslint no-use-before-define: "warn" */
+import { ethers } from "hardhat";
+import chalk from "chalk";
 import fs from "fs";
+import { utils, Contract } from "ethers";
+import { hasPath } from "ramda";
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const main = async () => {
+  console.log("\n\n 📡 Deploying...\n");
 
-  fs.unlinkSync(`${config.paths.artifacts}/contracts/contractAddress.ts`);
-
-  // We get the contract to deploy
-  const YourContract = await ethers.getContractFactory("Greeter");
-  const contract = await YourContract.deploy("Hello, Pukiti!");
-  await contract.deployed();
-  saveFrontendFiles(contract, "YourContract");
-  console.log("YourContract deployed to:", contract.address);
-
-  const MulticallContract = await ethers.getContractFactory("Multicall");
-  const multicallContract = await MulticallContract.deploy();
-  await multicallContract.deployed();
-  saveFrontendFiles(multicallContract, "MulticallContract");
-  console.log("Multicall deployed to:", multicallContract.address);
-}
-
-// https://github.com/nomiclabs/hardhat-hackathon-boilerplate/blob/master/scripts/deploy.js
-function saveFrontendFiles(contract: Contract, contractName: string) {
-  fs.appendFileSync(
-    `${config.paths.artifacts}/contracts/contractAddress.ts`,
-    `export const ${contractName} = '${contract.address}'\n`
+  // read in all the assets to get their IPFS hash...
+  let uploadedAssets = JSON.parse(
+    fs.readFileSync("./uploaded.json") as unknown as string
   );
-}
+  let bytes32Array = [];
+  for (let a in uploadedAssets) {
+    console.log(" 🏷 IPFS:", a);
+    let bytes32 = utils.id(a);
+    console.log(" #️⃣ hashed:", bytes32);
+    bytes32Array.push(bytes32);
+  }
+  console.log(" \n");
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+  // deploy the contract with all the artworks forSale
+  const yourCollectible = await deploy("YourCollectible", [bytes32Array]); // <-- add in constructor args like line 19 vvvv
+  const auction = await deploy("Auction", []); // <-- add in constructor args like line 19 vvvv
+
+  console.log(
+    " 💾  Artifacts (address, abi, and args) saved to: ",
+    chalk.blue("packages/hardhat/artifacts/"),
+    "\n\n"
+  );
+};
+
+const deploy = async (
+  contractName: string,
+  _args: string[][] = [],
+  overrides = {},
+  libraries = {}
+) => {
+  console.log(` 🛰  Deploying: ${contractName}`);
+
+  const contractArgs = _args || [];
+  const contractArtifacts = await ethers.getContractFactory(contractName, {
+    libraries: libraries,
+  });
+  const deployed = await contractArtifacts.deploy(...contractArgs, overrides);
+  const encoded = abiEncodeArgs(deployed, contractArgs);
+
+  let extraGasInfo = "";
+  if (
+    deployed &&
+    deployed.deployTransaction &&
+    deployed.deployTransaction.gasPrice
+  ) {
+    const gasUsed = deployed.deployTransaction.gasLimit.mul(
+      deployed.deployTransaction.gasPrice
+    );
+    extraGasInfo = `${utils.formatEther(gasUsed)} ETH, tx hash ${
+      deployed.deployTransaction.hash
+    }`;
+  }
+
+  console.log(
+    " 📄",
+    chalk.cyan(contractName),
+    "deployed to:",
+    chalk.magenta(deployed.address)
+  );
+  console.log(" ⛽", chalk.grey(extraGasInfo));
+
+  return deployed;
+};
+
+// ------ utils -------
+
+// abi encodes contract arguments
+// useful when you want to manually verify the contracts
+// for example, on Etherscan
+const abiEncodeArgs = (deployed: Contract, contractArgs: string[][]) => {
+  // not writing abi encoded args if this does not pass
+  if (
+    !contractArgs ||
+    !deployed ||
+    !hasPath(["interface", "deploy"], deployed)
+  ) {
+    return "";
+  }
+  const encoded = utils.defaultAbiCoder.encode(
+    deployed.interface.deploy.inputs,
+    contractArgs
+  );
+  return encoded;
+};
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
